dba4e1612f2a5b466e9441bb58218a129406704a
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2026-03-09 19:33:09 +0700

    fix

--- src/scoped_ptr.hpp
+++ src/scoped_ptr.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <algorithm>
 #include <utility>
+
 template <typename T>
 class ScopedPtr {
 protected:
@@ -12,11 +13,18 @@ public:
   virtual ~ScopedPtr() { delete raw; }
   T* get() { return raw; }
   const T* get() const { return raw; }
+  T& operator*() { return *raw; }
+  const T& operator*() const {return *raw; }
+  T* operator->() { return raw; }
+  const T* operator->() const { return raw; }
 };
 
 template <typename T>
 class CloningPtr : public ScopedPtr<T> {
 public:
+  CloningPtr() : ScopedPtr<T>() {};
+  CloningPtr(T* raw) : ScopedPtr<T>(raw) {};
+  
   CloningPtr& operator=(const ScopedPtr<T>& other) {
     T* ptr = other.raw ? new T(*other.raw) : nullptr;
     if (this->raw)
@@ -33,6 +41,9 @@ public:
 template <typename T>
 class UniquePtr : public ScopedPtr<T> {
 public:
+  UniquePtr() : ScopedPtr<T>() {};
+  UniquePtr(T* raw) : ScopedPtr<T>(raw) {};
+  
   UniquePtr& operator=(ScopedPtr<T>&& other) {
     T* ptr = std::exchange(other.raw, nullptr);
     if (this->raw)
