f7eecb30e70a516254ff74ed5016c40d410441c2
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2026-03-20 21:10:30 +0700

    nstt6

---
+++ src/matrix.hpp
@@ -0,0 +1,121 @@
+#pragma once
+#include <algorithm>
+#include <cassert>
+#include <concepts>
+#include <numeric>
+#include <span>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+template <typename T>
+class Matrix {
+  int rows_;
+  int cols_;
+  std::vector<T> mem_;
+  template <typename V>
+  friend class Matrix;
+
+public:
+  Matrix(const Matrix&) = default;
+  Matrix(Matrix&&) = default;
+  Matrix& operator=(const Matrix&) = default;
+  Matrix& operator=(Matrix&&) = default;
+
+  Matrix(T initial, int rows, int cols)
+      : rows_(rows), cols_(cols), mem_(rows * cols, initial) {}
+
+  Matrix(int rows, int cols) : rows_(rows), cols_(cols), mem_(rows * cols) {}
+
+  Matrix(const std::vector<T>& diagonal)
+      : rows_(diagonal.size()), cols_(diagonal.size()), mem_(rows_ * cols_) {
+    for (size_t i = 0; i < diagonal.size(); i++)
+      (*this)[i][i] = diagonal[i];
+  }
+
+  int rows() const { return rows_; }
+  int cols() const { return cols_; }
+
+  T& operator()(int index) { return mem_[index]; }
+  const T& operator()(int index) const { return mem_[index]; }
+  std::span<T> operator[](int index) {
+    return std::span<T>(mem_).subspan(index * cols_, cols_);
+  }
+  std::span<const T> operator[](int index) const {
+    return std::span<const T>(mem_).subspan(index * cols_, cols_);
+  }
+
+  explicit operator T() const {
+    return std::accumulate(mem_.begin(), mem_.end(), T());
+  }
+
+  template <std::invocable<const T&> F,
+            typename U = std::invoke_result_t<F, const T&>>
+  Matrix<U> map(F&& f) const {
+    Matrix<U> result(rows_, cols_);
+    std::transform(mem_.begin(), mem_.end(), result.mem_.begin(), f);
+    return result;
+  }
+
+  template <std::invocable<const T&> F>
+  Matrix<T>& transform(F&& f) {
+    std::transform(mem_.begin(), mem_.end(), mem_.begin(), std::forward(f));
+    return *this;
+  }
+
+  Matrix operator+(const Matrix& rhs) const {
+    if (rows_ != rhs.rows_ || cols_ != rhs.cols_) {
+      throw std::invalid_argument("matrix sizes are not the same");
+    }
+
+    Matrix result(rows_, cols_);
+    for (int i = 0; i < rows_ * cols_; i++) {
+      result.mem_[i] = mem_[i] + rhs.mem_[i];
+    }
+    return result;
+  }
+
+  Matrix& operator+=(const Matrix& rhs) {
+    *this = std::move(*this + rhs);
+    return *this;
+  }
+
+  Matrix operator+(const T& scalar) const {
+    return map([&scalar](const T& value) { return value + scalar; });
+  }
+
+  Matrix& operator+=(const T& scalar) {
+    return transform([&scalar](const T& value) { return value + scalar; });
+  }
+
+  Matrix operator*(const Matrix& rhs) const {
+    if (cols_ != rhs.rows_) {
+      throw std::invalid_argument(
+          "matrix sizes are not compatible for multiplication");
+    }
+
+    Matrix result(rows_, rhs.cols_);
+    for (int i = 0; i < rows_; i++) {
+      for (int j = 0; j < rhs.cols_; j++) {
+        for (int k = 0; k < cols_; k++) {
+          result[i][j] += (*this)[i][k] * rhs[k][j];
+        }
+      }
+    }
+    return result;
+  }
+
+  Matrix& operator*=(const Matrix& rhs) {
+    *this = std::move(*this * rhs);
+    return *this;
+  }
+
+  Matrix operator*(const T& scalar) const {
+    return map([&scalar](const T& value) { return value * scalar; });
+  }
+
+  Matrix& operator*=(const T& scalar) {
+    return transform([&scalar](const T& value) { return value * scalar; });
+  }
+};
---
+++ tests/test_matrix.cpp
@@ -0,0 +1,84 @@
+#include "src/matrix.hpp"
+#include <gtest/gtest.h>
+
+TEST(MatrixSuite, Simple) {
+  Matrix<int> matrix(42, 5, 10);
+  EXPECT_EQ(matrix.rows(), 5);
+  EXPECT_EQ(matrix.cols(), 10);
+  for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 10; j++)
+      EXPECT_EQ(matrix[i][j], 42);
+  matrix[1][1] = 43;
+  EXPECT_EQ(matrix[1][1], 43);
+  EXPECT_EQ(matrix(10 + 1), 43);
+}
+
+TEST(MatrixSuite, DiagonalCstr) {
+  std::vector<std::string> diag = {"DON'T", " ", "PANIC!"};
+  Matrix<std::string> matrix(diag);
+  EXPECT_EQ(matrix.rows(), 3);
+  EXPECT_EQ(matrix.cols(), 3);
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      if (i == j) {
+        EXPECT_EQ(matrix[i][j], diag[i]);
+      } else {
+        EXPECT_EQ(matrix[i][j], "");
+      }
+    }
+  }
+}
+
+TEST(MatrixSuite, Map) {
+  Matrix<int> m(42, 2, 2);
+  auto result = m.map([](const int& x) { return x * 10; });
+
+  EXPECT_EQ(result.rows(), 2);
+  EXPECT_EQ(result.cols(), 2);
+  EXPECT_DOUBLE_EQ(result[0][0], 420);
+  EXPECT_DOUBLE_EQ(result[0][1], 420);
+  EXPECT_DOUBLE_EQ(result[1][0], 420);
+  EXPECT_DOUBLE_EQ(result[1][1], 420);
+}
+
+TEST(MatrixSuite, PlusOperator) {
+  Matrix<int> a(1, 2, 3);
+  Matrix<int> b(42, 2, 3);
+  auto c = a + b;
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_EQ(c[i][j], a[i][j] + b[i][j]);
+    }
+  }
+  auto d = a + 1000;
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_EQ(d[i][j], a[i][j] + 1000);
+    }
+  }
+}
+
+TEST(MatrixSuite, MultipliesOperator) {
+  Matrix<int> a(1, 2, 3);
+  Matrix<int> b(2, 3, 2);
+  auto c = a * b;
+  EXPECT_EQ(c.rows(), 2);
+  EXPECT_EQ(c.cols(), 2);
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_EQ(c[i][j], 6);
+    }
+  }
+  auto d = a * 4;
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      EXPECT_EQ(d[i][j], a[i][j] * 4);
+    }
+  }
+}
+
+TEST(MatrixSuite, ExceptionOnInvalidMultiplication) {
+  Matrix<int> a(1, 2, 3);
+  Matrix<int> b(1, 4, 2);
+  EXPECT_THROW({ auto c = a * b; }, std::invalid_argument);
+}
